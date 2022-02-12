a32b05a10e055f08e6580ed5ad11bdacc0836a59
HaraldKangSchroeder <haraldkangschroederdevelopment@gmail.com>
2022-02-12 16:49:56 +0100

    update

--- logic/standard.py
+++ logic/standard.py
@@ -1,7 +1,9 @@
 import string
+from turtle import right
 from classes import Snake
 from classes.GameData import GameData
 from logic.enums.move import Move
+import random
 
 
 def handle_move(gamedata: GameData) -> string:
@@ -13,33 +15,45 @@ def handle_move(gamedata: GameData) -> string:
     up_position = {"x": my_head["x"], "y": (my_head["y"] + 1)}
     down_position = {"x": my_head["x"], "y": (my_head["y"] - 1)}
 
-    move = ""
+    possible_moves = []
 
-    if(is_position_free(gamedata, left_position)):
-        move = Move.left.value
-    elif(is_position_free(gamedata, right_position)):
-        move = Move.right.value
-    elif(is_position_free(gamedata, up_position)):
-        move = Move.up.value
-    elif(is_position_free(gamedata, down_position)):
-        move = Move.down.value
-    else:
-        move = Move.up.value
+    if(is_position_free(gamedata, left_position) and is_hazard_free(gamedata, left_position)):
+        possible_moves.append(Move.left.value)
+    if(is_position_free(gamedata, right_position) and is_hazard_free(gamedata, right_position)):
+        possible_moves.append(Move.right.value)
+    if(is_position_free(gamedata, up_position) and is_hazard_free(gamedata, up_position)):
+        possible_moves.append(Move.up.value)
+    if(is_position_free(gamedata, down_position) and is_hazard_free(gamedata, down_position)):
+        possible_moves.append(Move.down.value)
+
+    # just return smth when your destiny is to die anyway
+    if len(possible_moves) == 0:
+        return Move.left.value
+
+    move = random.choice(possible_moves)
 
     print(f"{gamedata.get_my_snake().get_id()} : {move}")
     return move
 
 
+def is_hazard_free(gamedata: GameData, new_position: dict) -> bool:
+    for position in gamedata.get_hazard_positions():
+        if position["x"] == new_position["x"] and position["y"] == new_position["y"]:
+            return False
+
+    return True
+
+
 def is_position_free(gamedata: GameData, new_position: dict) -> bool:
-    board_width = gamedata.get_board_width()
-    board_height = gamedata.get_board_height()
+    # board_width = gamedata.get_board_width()
+    # board_height = gamedata.get_board_height()
 
-    # check board collision
-    if new_position["x"] < 0 or new_position["x"] >= board_width:
-        return False
+    # # check board collision
+    # if new_position["x"] < 0 or new_position["x"] >= board_width:
+    #     return False
 
-    if new_position["y"] < 0 or new_position["y"] >= board_height:
-        return False
+    # if new_position["y"] < 0 or new_position["y"] >= board_height:
+    #     return False
 
     # check collisions with snakes
     if collides_with_snake(gamedata.get_my_snake(), new_position):
